1e7e54eb598d9ef7de91099bf7dc8b406321d267
slava <gloreus@gmail.com>
2016-03-14 18:26:52 +0300

    Показываем список проектов

--- app/data.py
+++ app/data.py
@@ -2,17 +2,29 @@ from git import Repo
 from git import InvalidGitRepositoryError, NoSuchPathError
 from flask import current_app
 import os
+import time
 
 
 def get_repo():
     path = current_app.config.get('REPO_ROOT', '')
 
-    dirs = [d for d in os.scandir(path) if d.is_dir]
+    dirs = [(os.path.join(path, d), d) for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
     res = []
-    for d in dirs:
+    for p, f in dirs:
         try:
-            repo = Repo(d.path)
-            res.append([repo.working_dir, repo.description])
+            repo = Repo(p)
+            rep_info = {}
+            if repo.refs:
+                try:
+                    commit = repo.commit()
+                    rep_info["author"] = commit.author
+                    rep_info["date"] = time.strftime('%Y-%m-%d %H:%M:%S%z', time.gmtime(commit.committed_date))
+                except ValueError:
+                    pass
+            rep_info["name"] = f
+            rep_info["desc"] = repo.description
+
+            res.append(rep_info)
         except (InvalidGitRepositoryError, NoSuchPathError) as e:
             print(e)
     return res
